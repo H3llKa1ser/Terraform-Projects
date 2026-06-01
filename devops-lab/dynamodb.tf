8845c12d0bc9cdb92070d23c02def8e3892593ce
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2026-06-01 14:33:16 +0200

    Create dynamodb.tf

---
+++ devops-lab/dynamodb.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "events" {
+  name         = "${var.project_name}-events"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "lab"
+  }
+}
