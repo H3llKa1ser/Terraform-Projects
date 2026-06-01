b5c384325e7c87b4ab8d47a7aad0c2b6ccf05023
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2026-06-01 14:32:56 +0200

    Create s3.tf

---
+++ devops-lab/s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "uploads" {
+  bucket = "${var.project_name}-uploads"
+}
+
+resource "aws_s3_bucket_versioning" "uploads" {
+  bucket = aws_s3_bucket.uploads.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
